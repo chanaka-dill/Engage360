fe508d8729a085d26ea8d9806f1c650485d59286
Chanaka Dilshan <chanakad633@gmail.com>
2025-03-05 22:32:43 +0530

    Back end project created

---
+++ Engage360/Backend Engage360/demo/src/main/java/com/example/demo/Engage360Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Engage360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Engage360Application.class, args);
+	}
+
+}
